33a44971cfea080072eb23c978dc70fdbae55501
mqobokazisinhle <mhalenisinhle@icloud.com>
2022-08-26 00:13:21 +0200

    fileupdate

--- learning_log1360/settings.py
+++ learning_log1360/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'learning_log1360.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
